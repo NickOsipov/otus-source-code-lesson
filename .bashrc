479fae73866fc76bc1bad06030585a3d9d7bdecc
NickOsipov <nick.osipov.91@gmail.com>
2024-09-30 00:53:12 +0300

    feat: CI/CD

---
+++ .bashrc
@@ -0,0 +1,8 @@
+YC_ZONE=ru-central1-a
+YC_SUBNET_NAME=default-ru-central1-a
+YC_SA_NAME=otus
+YC_USER=ubuntu
+
+function log() {
+    echo $(date) "| INFO:" $@
+}
---
+++ src/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+def main():
+    st.title("Hello World Streamlit")
+
+    name = st.text_input("Input your name:")
+    
+    if st.button("Say Hello"):
+        if name:
+            st.write(f"Hello, {name}!")
+        else:
+            st.write("Please input your name!")
+
+if __name__ == "__main__":
+    main()
