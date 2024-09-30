40f84a65e7aa8cccad2711787d60f9fe0a86165c
NickOsipov <nick.osipov.91@gmail.com>
2024-09-30 03:24:00 +0300

    fix

--- src/train.py
+++ src/train.py
@@ -2,6 +2,8 @@
 Main script to train and save the model.
 """
 
+from loguru import logger
+
 from src.data import load_data, split_data
 from src.models import (
     create_model,
@@ -22,27 +24,31 @@ def main() -> None:
     # pylint: disable=invalid-name
 
     # Load data
+    logger.info("Loading data...")
     x, y, feature_names, target_names = load_data()
 
     # Split data
+    logger.info("Splitting data...")
     x_train, x_test, y_train, y_test = split_data(x, y)
 
     # Create and train model
+    logger.info("Training model...")
     model = create_model()
     trained_model = train_model(model, x_train, y_train)
 
     # Evaluate model
+    logger.info("Evaluating model...")
     train_score, test_score = evaluate_model(
         trained_model, x_train, y_train, x_test, y_test
     )
-    print(f"Train score: {train_score:.2f}")
-    print(f"Test score: {test_score:.2f}")
-
+    logger.info(f"Train score: {train_score:.2f}")
+    logger.info(f"Test score: {test_score:.2f}")
+    
     # Save model and metadata
     save_model(trained_model)
     save_metadata(feature_names, target_names)
 
-    print("Model trained and saved successfully.")
+    logger.info("Model trained and saved successfully.")
 
 
 if __name__ == "__main__":
